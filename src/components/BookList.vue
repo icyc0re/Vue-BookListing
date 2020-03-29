f6a96e05bad0cc062858c280bdb80e153f40abd9
Tiziano Signo <ts@peterssoftware.de>
2020-03-29 17:39:32 +0200

    use BookItem component instead of "li" to visualize the books

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,6 +1,11 @@
 <script>
+import BookItem from './BookItem';
+
 export default {
     name: 'BookList',
+    components: {
+        BookItem,
+    },
     data() {
         return {
             title: 'All Books',
@@ -23,7 +28,7 @@ export default {
 <div>
     <h1>{{ title }}</h1>
     <ul>
-        <li v-for="book in books">{{ book.title }}: {{ book.author }}</li>
+        <book-item v-for="book in books" :book="book"></book-item>
     </ul>
 </div>
 </template>
