16a7a444423a01a63414fcdf8465f02087aa7394
Tiziano Signo <ts@peterssoftware.de>
2020-03-29 17:27:24 +0200

    minor refactor (and move li rule to the BookItem component)

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,12 +1,3 @@
-<template>
-<div>
-    <h1>{{ title }}</h1>
-    <ul>
-        <li v-for="book in books">{{ book.title }}: {{ book.author }}</li>
-    </ul>
-</div>
-</template>
-
 <script>
 export default {
     name: 'BookList',
@@ -28,6 +19,15 @@ export default {
 }
 </script>
 
+<template>
+<div>
+    <h1>{{ title }}</h1>
+    <ul>
+        <li v-for="book in books">{{ book.title }}: {{ book.author }}</li>
+    </ul>
+</div>
+</template>
+
 <style>
 h1,
 h2 {
@@ -36,9 +36,5 @@ h2 {
 ul {
     list-style-type: none;
     padding: 0;
-}
-li {
-    display: block;
-    margin: 0 10px;
 }
 </style>
