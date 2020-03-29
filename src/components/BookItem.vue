d3aeb760603abe4ba33ad04164455a7af6b25a64
Tiziano Signo <ts@peterssoftware.de>
2020-03-29 17:27:00 +0200

    add book item as component

---
+++ src/components/BookItem.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    name: 'BookItem',
+    props: 'book'
+}
+</script>
+
+<template>
+<li>{{ book.title }}: {{ book.author }}
+</template>
+
+<style>
+li {
+    display: block;
+    margin: 0 10px;
+}
+</style>
